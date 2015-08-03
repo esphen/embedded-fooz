0478216cdca0d16007f63c632e1b668b4086d1de
Espen Henriksen <dev@esphen.com>
2015-08-03 19:23:06 +0200

    Adds basic reading of pins to main script

--- embedded-fooz.sh
+++ embedded-fooz.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 echo
 echo ' _____ ___  ____   ___  _
 |_   _/ _ \|  _ \ / _ \| |
@@ -6,3 +7,28 @@ echo ' _____ ___  ____   ___  _
   |_| \___/|____/ \___/(_)'
 echo
 
+# Reads input on pin $INPUT
+
+INPUT=0
+OUTPUT=1
+LAST_VAL=0
+
+while true
+do
+  VALUE=$(gpio read $INPUT)
+  gpio write $OUTPUT $VALUE
+
+  # If signal input has changed
+  if [ $LAST_VALUE -ne $VALUE ]
+  then
+    if [ $VALUE -eq 1 ]
+    then
+      echo 'Input is detected'
+    else
+      echo 'Input signal no longer detected'
+    fi
+    LAST=$VALUE
+  fi
+done
+
+exit 0
